6b4b39859e1408eb4306546ead16dcb4511f053e
hemantjava <hemantjava90@gmail.com>
2020-11-18 15:25:40 +0530

    Timer Service.

--- src/main/java/com/hemant/aopexampleall/aspect/EmployeeAspect.java
+++ src/main/java/com/hemant/aopexampleall/aspect/EmployeeAspect.java
@@ -1,57 +1,58 @@
 package com.hemant.aopexampleall.aspect;
 
-import java.time.LocalDateTime;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.reflect.MethodSignature;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.time.LocalDate;
 
 @Log4j2
 @Aspect
 @Component
 public class EmployeeAspect {
 
-  // @Around("execution(* com.hemant.aopexampleall.service..*(..)))")
-  @Around("@annotation(LogExecutionTime)")
-  public void profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-    final String methodName = proceedingJoinPoint.getSignature().toShortString();
+    // @Around("execution(* com.hemant.aopexampleall.service..*(..)))")
+    @Around("@annotation(LogExecutionTime)")
+    public void profileAllMethods (ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        final Signature signature = proceedingJoinPoint.getSignature();
 
+        final String toLongString = proceedingJoinPoint.toLongString();
+        final String toString = proceedingJoinPoint.toString();
+        final String kind = proceedingJoinPoint.getKind();
 
-    //Measure method execution time
-    LocalDateTime st = LocalDateTime.now();
-    proceedingJoinPoint.proceed();
-    LocalDateTime ed = LocalDateTime.now();
+        //Measure method execution time
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        proceedingJoinPoint.proceed();
+        stopWatch.stop();
 
-    //Log method execution time
-    log.info("Execution dateAndTime of "+methodName+
-        "date " + st.toLocalDate() + " " + (ed.getNano() - st.getNano()) / 1000000 + " ms");
-  }
+        //Log method execution time
+        log.info("Execution dateAndTime of " + signature +
+                " date: " + LocalDate.now() + "  time: " + stopWatch.getTotalTimeMillis() + " ms");
+    }
 
-  @Before("execution(* com.hemant.aopexampleall.service..beforeAdvice()))")
-  public void before() {
-    log.info("before()");
-  }
+    @Before("execution(* com.hemant.aopexampleall.service..beforeAdvice()))")
+    public void before ( ) {
+        log.info("before()");
+    }
 
-  @After("execution(* com.hemant.aopexampleall.service..beforeAdvice()))")
-  public void after() {
-    log.info("after()");
-  }
+    @After("execution(* com.hemant.aopexampleall.service..beforeAdvice()))")
+    public void after ( ) {
+        log.info("after()");
+    }
 
-  @AfterThrowing("execution(* com.hemant.aopexampleall.service..getEpmThrowException())")
-  public void logExceptions(JoinPoint joinPoint){
-    log.info("Exception thrown in Employee Method="+joinPoint.getSignature().toShortString());
-  }
+    @AfterThrowing("execution(* com.hemant.aopexampleall.service..getEpmThrowException())")
+    public void logExceptions (JoinPoint joinPoint) {
+        log.info("Exception thrown in Employee Method=" + joinPoint.getSignature().toShortString());
+    }
 
-  @AfterReturning(pointcut="execution(* com.hemant.aopexampleall.service..getEpmName())", returning="returnString")
-  public void getNameReturningAdvice(String returnString){
-   log.error("getNameReturningAdvice executed. Returned String = "+returnString);
-  }
+    @AfterReturning(pointcut = "execution(* com.hemant.aopexampleall.service..getEpmName())", returning = "returnString")
+    public void getNameReturningAdvice (String returnString) {
+        log.error("getNameReturningAdvice executed. Returned String = " + returnString);
+    }
 
 }
--- src/main/java/com/hemant/aopexampleall/controller/EmployeeController.java
+++ src/main/java/com/hemant/aopexampleall/controller/EmployeeController.java
@@ -1,16 +1,22 @@
 package com.hemant.aopexampleall.controller;
 
 import com.hemant.aopexampleall.aspect.LogExecutionTime;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Log4j2
 @Controller
 public class EmployeeController {
 
+  @SneakyThrows
   @LogExecutionTime
+  @GetMapping("/")
   public void empAround(){
+    Thread.sleep(5000);
     log.info("EmployeeController");
+
   }
 
 }
