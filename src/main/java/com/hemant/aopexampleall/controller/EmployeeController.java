72e28a0ebd0ee4a9743927d9f268b5dde3a41981
hemantjava <hemantjava90@gmail.com>
2020-11-18 17:05:09 +0530

    Timer Service.

--- src/main/java/com/hemant/aopexampleall/controller/EmployeeController.java
+++ src/main/java/com/hemant/aopexampleall/controller/EmployeeController.java
@@ -4,19 +4,17 @@ import com.hemant.aopexampleall.aspect.LogExecutionTime;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Log4j2
 @Controller
 public class EmployeeController {
 
-  @SneakyThrows
-  @LogExecutionTime
-  @GetMapping("/")
-  public void empAround(){
-    Thread.sleep(5000);
-    log.info("EmployeeController");
+    @SneakyThrows
+    @LogExecutionTime
+    public void empAround ( ) {
+        Thread.sleep(5000);
+        log.info("EmployeeController");
 
-  }
+    }
 
 }
